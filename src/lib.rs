a9aae8ce178c991a1b6e091eda8b0150ab291bd2
Dmitry Ulyanov <sillent1987@gmail.com>
2024-11-17 16:04:16 +0300

    refactor reads functions as module

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
+pub mod read;
+
 use core::panic;
+use std::collections::HashMap;
 use std::error::Error;
-use std::io::BufReader;
-use std::io::Read;
+use std::io::{BufReader, Read};
 
 use x509_certificate::certificate::X509Certificate;
 
@@ -40,14 +42,21 @@ impl From<[u8; 4]> for EntryType {
 
 #[derive(Debug, PartialEq)]
 pub struct Store {
-    pub certs: Option<Vec<Cert>>,
+    pub certs: Option<Vec<CertInfo>>,
     pub key_pairs: Option<Vec<KeyPair>>,
 }
 
 #[derive(Debug, PartialEq, Eq)]
-pub struct Cert {
+pub struct CertInfo {
     pub alias: String,
     pub timestamp: u64,
+    pub certificate: Cert,
+    // pub raw: Vec<u8>,
+    // pub cert: X509Certificate,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct Cert {
     pub raw: Vec<u8>,
     pub cert: X509Certificate,
 }
@@ -58,45 +67,60 @@ pub struct KeyPair {
     pub timestamp: u64,
     pub encrypted_key: Vec<u8>,
     pub raw_key: Vec<u8>,
+    pub cert_chain: Vec<KeyPairCert>,
 }
 
 #[derive(Debug, PartialEq, Eq)]
-pub struct Instance {
-    pub alias: String,
-    pub timestamp: u64,
-    key: Option<Vec<u8>>,
-    cert: Vec<u8>,
+pub struct KeyPairCert {
+    pub raw: Vec<u8>,
+    pub cert: X509Certificate,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct Options {
+    pub password: String,
+    pub skip_verify: bool,
+    pub key_passwords: HashMap<String, String>,
+}
+
+impl Default for Options {
+    fn default() -> Self {
+        Options {
+            password: "changeit".to_owned(),
+            skip_verify: false,
+            key_passwords: HashMap::new(),
+        }
+    }
 }
 
 impl Store {
-    pub fn parse(data: impl AsRef<[u8]>, _password: String) -> Result<Self, Box<dyn Error>> {
+    pub fn parse(data: impl AsRef<[u8]>, opts: Option<Options>) -> Result<Self, Box<dyn Error>> {
         let mut buffer = BufReader::new(data.as_ref());
 
-        let magic = read_u32(&mut buffer)?;
+        let magic = read::read_u32(&mut buffer)?;
         if !magic.eq(&MAGIC) {
             return Err(format!(
                 "invalid file format, expected header '{:#x?}', but got '{:#x?}'",
                 MAGIC, magic
             ))?;
         }
-        let version = read_u32(&mut buffer)?;
+        let version = read::read_u32(&mut buffer)?;
         if Version::from(version) == Version::Unsupported {
             return Err("unsupported version, supported only version 2".to_owned())?;
         }
         let mut keypairs: Vec<KeyPair> = vec![];
-        let mut certs: Vec<Cert> = vec![];
+        let mut certs: Vec<CertInfo> = vec![];
+        let mut opts = opts.unwrap_or_default();
 
-        let entries = u32::from_be_bytes(read_u32(&mut buffer)?);
+        let entries = u32::from_be_bytes(read::read_u32(&mut buffer)?);
         for _ in 0..entries {
-            let entry_type = EntryType::from(read_u32(&mut buffer)?);
+            let entry_type = EntryType::from(read::read_u32(&mut buffer)?);
             match entry_type {
-                EntryType::KeyPair => unimplemented!("key pairs"),
-                EntryType::Certs => {
-                    let cert = process_cert(&mut buffer)?;
-                    certs.push(cert);
-                }
+                EntryType::KeyPair => keypairs.push(process_key_pair(&mut buffer, &mut opts)?),
+                EntryType::Certs => certs.push(process_cert(&mut buffer)?),
             }
         }
+
         Ok(Store {
             certs: Some(certs),
             key_pairs: Some(keypairs),
@@ -104,138 +128,45 @@ impl Store {
     }
 }
 
-fn process_cert<T>(data: &mut BufReader<T>) -> Result<Cert, Box<dyn Error>>
+fn process_cert<T>(data: &mut BufReader<T>) -> Result<CertInfo, Box<dyn Error>>
 where
     T: Read,
 {
-    let alias = read_str(data)?;
-    let timestamp = read_timestamp(data)?;
-    let cert_type = read_str(data)?;
-    if !cert_type.eq("X.509") {
-        return Err(format!("not supported certificate type: {}", cert_type))?;
-    }
-    let cert_length = read_u32(data)?;
-    let cert_der = read_bytes(data, u32::from_be_bytes(cert_length) as usize)?;
-    let parsed_cert = X509Certificate::from_der(cert_der.clone())?;
-    Ok(Cert {
+    let alias = read::read_str(data)?;
+    let timestamp = read::read_timestamp(data)?;
+    // let cert_type = read_str(data)?;
+    // if !cert_type.eq("X.509") {
+    //     return Err(format!("not supported certificate type: {}", cert_type))?;
+    // }
+    // let cert_length = read_u32(data)?;
+    // let cert_der = read_bytes(data, u32::from_be_bytes(cert_length) as usize)?;
+    // let parsed_cert = X509Certificate::from_der(cert_der.clone())?;
+    let certificate = read::read_cert(data)?;
+    // let certificate = Cert {
+    //     raw: cert_der,
+    //     cert: parsed_cert,
+    // };
+    Ok(CertInfo {
         alias,
         timestamp,
-        raw: cert_der,
-        cert: parsed_cert,
+        certificate,
+        // raw: cert_der,
+        // cert: parsed_cert,
     })
 }
 
-fn read_bytes<T>(data: &mut BufReader<T>, len: usize) -> Result<Vec<u8>, String>
+fn process_key_pair<T>(
+    data: &mut BufReader<T>,
+    opts: &mut Options,
+) -> Result<KeyPair, Box<dyn Error>>
 where
     T: Read,
 {
-    use std::io::ErrorKind::UnexpectedEof;
-    let mut buf: Vec<u8> = vec![0; len];
-    match data.read_exact(&mut buf) {
-        Ok(_) => Ok(buf),
-        Err(e) if e.kind() == UnexpectedEof => Err(format!(
-            "buffer is too short, at least {} are required",
-            len
-        )),
-        Err(e) => Err(format!("error reading bytes: {e}")),
-    }
-}
+    let alias = read::read_str(data)?;
+    let timestamp = read::read_timestamp(data)?;
+    let password = opts.key_passwords.get(&alias).unwrap_or(&opts.password);
+    let key_length = read::read_u32(data)?;
+    let key = read::read_bytes(data, u32::from_be_bytes(key_length) as usize)?;
 
-fn read_u16<T>(data: &mut BufReader<T>) -> Result<[u8; 2], String>
-where
-    T: Read,
-{
-    use std::io::ErrorKind::UnexpectedEof;
-    let mut buf = [0u8; 2];
-    match data.read_exact(&mut buf) {
-        Ok(_) => Ok(buf),
-        Err(e) if e.kind() == UnexpectedEof => {
-            Err("buffer is too short, at least 2 bytes are required".to_owned())
-        }
-        Err(e) => Err(format!("error reading bytes: {}", e)),
-    }
-}
-
-fn read_u32<T>(data: &mut BufReader<T>) -> Result<[u8; 4], String>
-where
-    T: Read,
-{
-    use std::io::ErrorKind::UnexpectedEof;
-    let mut buf = [0u8; 4];
-    match data.read_exact(&mut buf) {
-        Ok(_) => Ok(buf),
-        Err(e) if e.kind() == UnexpectedEof => {
-            Err("buffer is too short, at least 4 bytes are required".to_string())
-        }
-        Err(e) => Err(format!("error reading bytes: {}", e)),
-    }
-}
-
-fn read_u64<T>(data: &mut BufReader<T>) -> Result<[u8; 8], String>
-where
-    T: Read,
-{
-    use std::io::ErrorKind::UnexpectedEof;
-    let mut buf = [0u8; 8];
-    match data.read_exact(&mut buf) {
-        Ok(_) => Ok(buf),
-        Err(e) if e.kind() == UnexpectedEof => {
-            Err("buffer is too short, at least 8 bytes are required".to_string())
-        }
-        Err(e) => Err(format!("error reading bytes: {}", e)),
-    }
-}
-
-fn read_str<T>(data: &mut BufReader<T>) -> Result<String, String>
-where
-    T: Read,
-{
-    let length = u16::from_be_bytes(read_u16(data)?);
-    let mut buf: Vec<u8> = vec![0; length as usize];
-    match data.read_exact(&mut buf) {
-        Ok(()) => {}
-        Err(e) => return Err(format!("failed to read string: {}", e)),
-    }
-    match String::from_utf8(buf) {
-        Ok(res) => Ok(res),
-        Err(e) => Err(format!("{}", e)),
-    }
-}
-
-fn read_timestamp<T>(data: &mut BufReader<T>) -> Result<u64, String>
-where
-    T: Read,
-{
-    let timestamp = read_u64(data)?;
-    Ok(u64::from_be_bytes(timestamp))
-}
-
-#[cfg(test)]
-mod tests {
-    use std::io;
-
-    use super::*;
-
-    #[test]
-    fn four_bytes_readed() {
-        let data = [0xfeu8, 0xed, 0xfe, 0xed, 0x00];
-        let cursor = io::Cursor::new(data);
-        let mut buf = BufReader::new(cursor);
-        let four_bytes = read_u32(&mut buf).unwrap();
-        assert_eq!(four_bytes.len(), 4);
-        assert_eq!(four_bytes, MAGIC);
-        // check buffer contain last 1 bytes
-        let mut other = [0u8; 4];
-        assert_eq!(buf.read(&mut other).unwrap(), 1);
-    }
-
-    #[test]
-    fn version() {
-        let data = [0x00u8, 0x00, 0x00, 0x02];
-        let cursor = io::Cursor::new(data);
-        let mut buf = BufReader::new(cursor);
-        let four_bytes = read_u32(&mut buf).unwrap();
-        let d = u32::from_be_bytes(four_bytes);
-        assert_eq!(d, 2);
-    }
+    unimplemented!("key pairs")
 }
