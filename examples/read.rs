352568bb47390a5bb8fd255b1d4b338e0f7e91ba
Dmitry Ulyanov <sillent1987@gmail.com>
2024-11-18 23:23:57 +0300

    keypair parsing and examples

--- examples/read.rs
+++ examples/read.rs
@@ -4,15 +4,22 @@ use minijks;
 
 fn main() {
     let mut buf = vec![];
-    let mut f = File::open("examples/file.jks").unwrap();
+    let mut f = File::open("examples/store-example.jks").unwrap();
     f.read_to_end(&mut buf).unwrap();
     let certs = minijks::Store::parse(&buf, None).unwrap();
-    for cert in certs.certs.unwrap() {
+    for cert in certs.certs {
         println!(
             "cert alias = {}, cert cn = {:?}, and serial = {:?}",
             cert.alias,
             cert.certificate.cert.subject_common_name(),
             cert.certificate.cert.serial_number_asn1(),
         )
+    }
+    for keyp in certs.key_pairs {
+        println!(
+            "keypair alias = {} len = {}",
+            keyp.alias,
+            keyp.encrypted_key.len()
+        );
     }
 }
